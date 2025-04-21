42cd3bea0ecefc49b55e2b132945d2b7aea44012
Patrick Zimmermann <patrick01.zimmermann@tum.de>
2025-04-21 16:08:48 +0200

    first draft metrics without joy

--- Assets/Code/Patrick/EmotionMockUp.cs
+++ Assets/Code/Patrick/EmotionMockUp.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public struct emotionJumpParameters
@@ -5,18 +7,44 @@ public struct emotionJumpParameters
     private float jumpStrenght;
     private float fallStrength;
     private float gravity;
+
+    public emotionJumpParameters(float jumpStrenght,float fallStrength, float gravity)
+    {
+        this.jumpStrenght = jumpStrenght;
+        this.fallStrength = fallStrength;
+        this.gravity = gravity;
+    }
 }
 
 public class EmotionMockUp : MonoBehaviour
 {
     public GameObject playerObj;
     public CharacterControllerMockup controler;
+    private Dictionary<String,emotionJumpParameters> emotionParameters = new Dictionary<String, emotionJumpParameters>();
     
-    
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        emotionJumpParameters defaultparams = new emotionJumpParameters(18,70,10);
+        emotionJumpParameters joyParams = new emotionJumpParameters(25, 50, 10);
+        emotionParameters.Add("Default", defaultparams);
+        emotionParameters.Add("Joy", joyParams);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            applyConfig(controler, emotionParameters["Default"]);
+        }
+        else if(Input.GetKeyUp(KeyCode.E))
+        {
+            applyConfig(controler, emotionParameters["Joy"]);
+        }
+    }
+
+    private void applyConfig(CharacterControllerMockup controller, emotionJumpParameters paramsToApply)
+    {
+           //TODO:
+    }
+    
 }
