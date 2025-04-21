094a6be1dad2a135cc5489a35bf06a7d946a9b45
Patrick Zimmermann <patrick01.zimmermann@tum.de>
2025-04-21 19:19:55 +0200

    added mockup for emotion change system, untested

--- Assets/Code/Patrick/EmotionMockUp.cs
+++ Assets/Code/Patrick/EmotionMockUp.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public struct emotionJumpParameters
 {
@@ -18,8 +19,17 @@ public struct emotionJumpParameters
 
 public class EmotionMockUp : MonoBehaviour
 {
+    [Header("Character Controller Paramerter")]
     public CharacterControllerMockup controler;
     private Dictionary<String,emotionJumpParameters> emotionParameters = new Dictionary<String, emotionJumpParameters>();
+    
+    [Header("Emotion Change Mockup")]
+    [SerializeField] public UnityEvent Joy;
+    [SerializeField] public UnityEvent Fear;
+    [SerializeField] public UnityEvent Lonely;
+
+    [Header("Emotions")] 
+    public List<GameObject> lonelyObjects;
     
     private void Start()
     {
@@ -29,18 +39,36 @@ public class EmotionMockUp : MonoBehaviour
         emotionParameters.Add("Joy", joyParams);
     }
 
+    private void Awake()
+    {
+        Joy.AddListener(OnJoy);
+        Lonely.AddListener(OnLonely);
+        Fear.AddListener(OnFear);
+    }
+
+    private void OnDestroy()
+    {
+        Joy.RemoveListener(OnJoy);
+        Lonely.RemoveListener(OnLonely);
+        Fear.RemoveListener(OnFear);
+    }
+
     private void Update()
     {
+        //Input Change
         if (Input.GetKeyUp(KeyCode.C))
         {
-            Debug.Log("Loading Default Config");
-            applyConfig(controler, emotionParameters["Default"]);
+            restoreDefaultConfig();
         }
         else if(Input.GetKeyUp(KeyCode.E))
         {
-            Debug.Log("Loading Joy config");
-            applyConfig(controler, emotionParameters["Joy"]);
+            OnJoy();
         }
+        
+        //Emotion Change
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Joy.Invoke();
+        if (Input.GetKeyDown(KeyCode.Alpha2)) Fear.Invoke();
+        if (Input.GetKeyDown(KeyCode.Alpha3)) Lonely.Invoke();
     }
 
     private void applyConfig(CharacterControllerMockup controller, emotionJumpParameters paramsToApply)
@@ -49,5 +77,48 @@ public class EmotionMockUp : MonoBehaviour
         controller.jumpStrength = paramsToApply.jumpStrenght;
         controller.fallStrength = paramsToApply.fallStrength;
     }
-    
+
+    private void OnJoy()
+    {
+        enableColliders();
+        Debug.Log("Loading Joy config");
+        applyConfig(controler, emotionParameters["Joy"]);
+    }
+
+    private void OnLonely()
+    {
+        restoreDefaultConfig();
+        //Debug.Log("Not implemented, need objects to deactivate collider from!");
+        disableColliders();
+    }
+
+    private void OnFear()
+    {
+        enableColliders();
+        restoreDefaultConfig();
+    }
+
+    private void restoreDefaultConfig()
+    {
+        Debug.Log("Loading Default Config");
+        applyConfig(controler, emotionParameters["Default"]);
+    }
+
+    private void disableColliders()
+    {
+        foreach (GameObject o in lonelyObjects)
+        {
+            var component = o.GetComponent<Collider>();
+            if (component != null) component.enabled = false;
+        }
+    }
+
+    private void enableColliders()
+    {
+        foreach (GameObject o in lonelyObjects)
+        {
+            var component = o.GetComponent<Collider>();
+            if (component != null) component.enabled = true;
+        }
+    }
 }
