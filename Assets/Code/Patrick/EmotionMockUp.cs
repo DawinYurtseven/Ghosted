826defe16122aabb390c35a79ce20aa30f70324e
Patrick Zimmermann <patrick01.zimmermann@tum.de>
2025-04-22 11:15:24 +0200

    added some tools from Patrick and made a mockup of a puzzle

--- Assets/Code/Patrick/EmotionMockUp.cs
+++ Assets/Code/Patrick/EmotionMockUp.cs
@@ -17,9 +17,17 @@ public struct emotionJumpParameters
     }
 }
 
+public enum RoomState
+{
+    Normal,Joy, Fear, Lonely
+}
+
 public class EmotionMockUp : MonoBehaviour
 {
+    private RoomState _roomState = RoomState.Normal;
+    
     [Header("Character Controller Paramerter")]
+    public GameObject playerObj;
     public CharacterControllerMockup controler;
     private Dictionary<String,emotionJumpParameters> emotionParameters = new Dictionary<String, emotionJumpParameters>();
     
@@ -27,10 +35,15 @@ public class EmotionMockUp : MonoBehaviour
     [SerializeField] public UnityEvent Joy;
     [SerializeField] public UnityEvent Fear;
     [SerializeField] public UnityEvent Lonely;
+    [SerializeField] public UnityEvent Bind;
 
     [Header("Emotions")] 
     public List<GameObject> lonelyObjects;
-    
+    public List<Transform> teleportPos;
+    public Material transparentMat;
+    public Material ogMat;
+    [SerializeField]private bool isBound = false;
+    [SerializeField]private GameObject boundObj;
     private void Start()
     {
         emotionJumpParameters defaultparams = new emotionJumpParameters(18,70,10);
@@ -38,6 +51,8 @@ public class EmotionMockUp : MonoBehaviour
         emotionParameters.Add("Default", defaultparams);
         emotionParameters.Add("Joy", joyParams);
     }
+
+    #region Subscribe
 
     private void Awake()
     {
@@ -53,6 +68,8 @@ public class EmotionMockUp : MonoBehaviour
         Fear.RemoveListener(OnFear);
     }
 
+    #endregion
+    
     private void Update()
     {
         //Input Change
@@ -69,6 +86,7 @@ public class EmotionMockUp : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1)) Joy.Invoke();
         if (Input.GetKeyDown(KeyCode.Alpha2)) Fear.Invoke();
         if (Input.GetKeyDown(KeyCode.Alpha3)) Lonely.Invoke();
+        if(Input.GetKeyDown(KeyCode.Q)) Bind.Invoke();
     }
 
     private void applyConfig(CharacterControllerMockup controller, emotionJumpParameters paramsToApply)
@@ -80,13 +98,31 @@ public class EmotionMockUp : MonoBehaviour
 
     private void OnJoy()
     {
+        Debug.Log("Changing to Joy");
+        _roomState = RoomState.Joy;
+        
         enableColliders();
+        
+        if (isBound && boundObj)
+        {
+            disableCollider(boundObj);
+            addTransparency(boundObj);
+        }
+        else if(boundObj)
+        {
+            enableCollider(boundObj);
+            removeTransparency(boundObj);
+        }
+        
         Debug.Log("Loading Joy config");
         applyConfig(controler, emotionParameters["Joy"]);
     }
 
     private void OnLonely()
     {
+        Debug.Log("Changing to Lonely");
+        _roomState = RoomState.Lonely;
+        
         restoreDefaultConfig();
         //Debug.Log("Not implemented, need objects to deactivate collider from!");
         disableColliders();
@@ -94,6 +130,9 @@ public class EmotionMockUp : MonoBehaviour
 
     private void OnFear()
     {
+        Debug.Log("Changing to Fear");
+        _roomState = RoomState.Fear;
+        
         enableColliders();
         restoreDefaultConfig();
     }
@@ -108,17 +147,97 @@ public class EmotionMockUp : MonoBehaviour
     {
         foreach (GameObject o in lonelyObjects)
         {
-            var component = o.GetComponent<Collider>();
-            if (component != null) component.enabled = false;
+            disableCollider(o);
+            addTransparency(o);
         }
     }
 
+    public void disableCollider(GameObject o)
+    {
+        var component = o.GetComponent<Collider>();
+        if (component != null) component.enabled = false;
+    }
+
+    public void enableCollider(GameObject o)
+    {
+        var component = o.GetComponent<Collider>();
+        if (component != null) component.enabled = true;
+    }
+    
     private void enableColliders()
     {
         foreach (GameObject o in lonelyObjects)
         {
-            var component = o.GetComponent<Collider>();
-            if (component != null) component.enabled = true;
+            enableCollider(o);
+            removeTransparency(o);
         }
+    }
+
+    private void addTransparency(GameObject o)
+    {
+        Renderer renderer = o.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = transparentMat;
+        }
+    }
+
+    private void removeTransparency(GameObject o)
+    {
+        Renderer renderer = o.GetComponent<Renderer>();
+        if (renderer != null && ogMat != null)
+        {
+            renderer.material = ogMat;
+        }
+    }
+    
+    public void teleport(Transform newPos)
+    {
+        playerObj.transform.position = newPos.position;
+    }
+
+    public void changeRoomstate(RoomState newState)
+    {
+        _roomState = newState;
+    }
+    
+    public void BindObj(GameObject obj)
+    {
+        switch (_roomState)
+        {
+            case RoomState.Lonely:
+                Debug.Log("Binding obj in Lonely");
+                if (lonelyObjects.Contains(obj))
+                {
+                    disableCollider(obj);
+                    Debug.Log("Disabled " + obj);
+                }
+                    
+                break;
+            
+            default:
+                Debug.Log("Bound obj" + obj);
+                break;
+        }
+    }
+
+    public void toggleBind(GameObject gameObject)
+    {
+        boundObj = gameObject;
+        isBound = !isBound;
+        
+        if (isBound)
+        {
+            Debug.Log("Obj bound");
+            disableCollider(gameObject);
+            addTransparency(gameObject);
+        }
+        else
+        {
+            Debug.Log("Obj unbound");
+            enableCollider(gameObject);
+            removeTransparency(gameObject);
+        }
+        
     }
 }
