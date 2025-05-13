ea4aeeda8ab9be371d5b16f19b4500c8b2a5cc2f
Patrick Zimmermann <patrick01.zimmermann@tum.de>
2025-05-14 01:46:50 +0200

    just for safety, commented out code that is definitly not needed tomorrow

--- Assets/Code/Patrick/DoorFear.cs
+++ Assets/Code/Patrick/DoorFear.cs
@@ -2,12 +2,12 @@ public class DoorFear : FearObjectParent
 {
     public TalismanTargetMock TargetMock;
     
-    public override void Lock() {
-        _locked = !_locked;
-        if (specialEffect != null)
-        {
-            specialEffect.SetActive(true);
-        }
-    }
+    // public override void Lock() {
+    //     _locked = !_locked;
+    //     if (specialEffect != null)
+    //     {
+    //         specialEffect.SetActive(true);
+    //     }
+    // }
     
 }
