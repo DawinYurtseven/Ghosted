e2df54ae3c9c4df69c340ebe8a00e5fb1884c51a
bentoBAUX <bennettpoh@gmail.com>
2025-06-03 23:10:10 +0200

    Bind VFX Complete

---
+++ Assets/Code/Ben/Scripts/VFX/ExampleVFXUsage.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ExampleVFXUsage : MonoBehaviour
+{
+    public GameObject vfxPrefab;
+    public Transform target;
+
+    public Emotion emotions;
+    // Update is called once per frame
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var vfx = Instantiate(vfxPrefab).GetComponent<VFXController>();
+            vfx.PlayAt(target, emotions);
+        }
+
+    }
+}
---
+++ Assets/Code/Ben/Scripts/VFX/VFXController.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VFXController : MonoBehaviour
+{
+    private VisualEffect vfx;
+    [Header("Base Colours")][ Space(10)]
+    [ColorUsage(true, true)] public Color joyColour;
+    [ColorUsage(true, true)] public Color fearColour;
+    [ColorUsage(true, true)] public Color lonelinessColour;
+    [ColorUsage(true, true)] public Color loveColour;
+
+    [Header("Foreground Gradients")][ Space(10)]
+    public Gradient joyFgGradient;
+    public Gradient fearFgGradient;
+    public Gradient lonelinessFgGradient;
+    public Gradient loveFgGradient;
+
+    [Header("Background Gradients")][ Space(10)]
+    public Gradient joyBgGradient;
+    public Gradient fearBgGradient;
+    public Gradient lonelinessBgGradient;
+    public Gradient loveBgGradient;
+
+    [Header("Emotion Character")] [Space(10)]
+    public Texture2D joyTex2D;
+    public Texture2D fearTex2D;
+    public Texture2D lonelinessTex2D;
+    public Texture2D loveTex2D;
+
+    [Range(0, 1)] public float clipAmount = 0.4f;
+    public float clipSpeed = 1f;
+    float clip = 1;
+
+    private void OnEnable()
+    {
+        vfx = GetComponent<VisualEffect>();
+        clip = 1;
+    }
+
+    public void PlayAt(Transform parent, Emotion emotion)
+    {
+        transform.position = parent.position;
+        transform.rotation = parent.rotation;
+
+        vfx.SetVector4("BaseColour", GetEmotionColour(emotion));
+        vfx.SetGradient("PulseFGGradient", GetEmotionFgGradient(emotion));
+        vfx.SetGradient("PulseBGGradient", GetEmotionBgGradient(emotion));
+        vfx.SetTexture("CharacterTexture", joyTex2D);
+        vfx.SetVector3("VFXMeshScale", parent.transform.lossyScale);
+
+        transform.SetParent(parent);
+        vfx.SendEvent("OnPlay");
+        clip = 1;
+    }
+
+    private Color GetEmotionColour(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Joy:
+                return joyColour;
+            case Emotion.Fear:
+                return fearColour;
+            case Emotion.Lonely:
+                return lonelinessColour;
+            case Emotion.Love:
+                return loveColour;
+            default:
+                return joyColour;
+        }
+    }
+
+    private Gradient GetEmotionFgGradient(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Joy:
+                return joyFgGradient;
+            case Emotion.Fear:
+                return fearFgGradient;
+            case Emotion.Lonely:
+                return lonelinessFgGradient;
+            case Emotion.Love:
+                return loveFgGradient;
+            default:
+                return joyFgGradient;
+        }
+    }
+
+    private Gradient GetEmotionBgGradient(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Joy:
+                return joyBgGradient;
+            case Emotion.Fear:
+                return fearBgGradient;
+            case Emotion.Lonely:
+                return lonelinessBgGradient;
+            case Emotion.Love:
+                return loveBgGradient;
+            default:
+                return joyBgGradient;
+        }
+    }
+
+    private Texture2D GetEmotionTex2D(Emotion emotion)
+    {
+        switch (emotion)
+        {
+            case Emotion.Joy:
+                return joyTex2D;
+            case Emotion.Fear:
+                return fearTex2D;
+            case Emotion.Lonely:
+                return lonelinessTex2D;
+            case Emotion.Love:
+                return loveTex2D;
+            default:
+                return joyTex2D;
+        }
+    }
+    private void Update()
+    {
+        clip = Mathf.MoveTowards(clip, clipAmount, Time.deltaTime * clipSpeed);
+        vfx.SetFloat("Clip", clip);
+    }
+
+}
