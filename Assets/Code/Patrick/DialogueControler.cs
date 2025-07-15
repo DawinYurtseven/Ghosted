b82ca7c26193d9d9081ac9379b350cf03832f700
Patrick Zimmermann <patrick.zimmermann01@o2mail.de>
2025-07-15 22:04:53 +0200

    camera movement in first half of dialogue

--- Assets/Code/Patrick/DialogueControler.cs
+++ Assets/Code/Patrick/DialogueControler.cs
@@ -1,6 +1,24 @@
+using Ghosted.Dialogue;
 using UnityEngine;
 
-public class DialogueCont : MonoBehaviour
+public class DialogueControler : MonoBehaviour
 {
+    [Header("Going into Ghost Lock")]
+    public AIConversant ghostConversant;
+    public PlayerConversant playerConversant;
+    public ghostOrb ghostOrb;
+    public Transform ghostTarget;
     
+    public void forceDialogueStart(bool fastMove = false)
+    {
+        if (ghostConversant == null || playerConversant == null ) return;
+        if(ghostTarget == null || ghostOrb == null) return;
+        
+        if(!fastMove)
+            ghostOrb.move2TransformSlow(ghostTarget);
+        else
+            ghostOrb.MoveToTransform(ghostTarget);
+        
+        ghostConversant.Interact(playerConversant);
+    }
 }
