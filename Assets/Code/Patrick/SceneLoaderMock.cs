eb1cc0b494a8df64cf8b97281a74dd6c5b6a8e16
Alexandra Menshikova <ochlinetime@gmail.com>
2025-07-16 13:30:43 +0200

    StartScene pointer fix

--- Assets/Code/Patrick/SceneLoaderMock.cs
+++ Assets/Code/Patrick/SceneLoaderMock.cs
@@ -1,11 +1,17 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SceneLoaderMock : MonoBehaviour
 {
     public String introSceneName;
     public String trainSceneName;
-    
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void loadIntroScene()
     {
         if (string.IsNullOrEmpty(introSceneName))
