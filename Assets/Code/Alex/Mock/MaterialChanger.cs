0c56e5711974628ca1d926e751cb30ecad41fb0f
bentoBAUX <bennettpoh@gmail.com>
2025-08-19 11:34:45 +0200

    Added fear materials part 1

--- Assets/Code/Alex/Mock/MaterialChanger.cs
+++ Assets/Code/Alex/Mock/MaterialChanger.cs
@@ -1,40 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UniRx;
+
 public class MaterialChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Material joyMaterial, fearMaterial;
     private Renderer _renderer;
-    
+    private System.IDisposable _sub;
+
     void Start()
     {
-        
         _renderer = gameObject.GetComponent<MeshRenderer>();
         if (!_renderer) return;
+
         _renderer.material = joyMaterial;
-        if (!_renderer) return;
+
         EmotionSingletonMock.Instance.EmotionSubject
-            .Subscribe(emotion =>
-            {
-                ChangeMaterial(emotion);
-            });
+            .Subscribe(emotion => { ChangeMaterial(emotion); });
+
         ChangeMaterial(EmotionSingletonMock.Instance.getCurrentEmotion());
     }
 
     public void ChangeMaterial(Emotion emotion)
     {
-        switch (emotion)
+        if (!_renderer) _renderer = GetComponent<Renderer>();
+        if (!_renderer) return;
+
+        var mat = (emotion == Emotion.Joy) ? joyMaterial : fearMaterial;
+
+        // Use sharedMaterial in Edit Mode to avoid instancing
+        if (!Application.isPlaying)
         {
-            case Emotion.Joy: 
-                _renderer.material = joyMaterial;
-                break;
-            case Emotion.Fear:
-                _renderer.material = fearMaterial;
-                break;
-            default:
-                break;
+            if (_renderer.sharedMaterials.Length > 1)
+            {
+                _renderer.materials = Enumerable.Repeat(mat, _renderer.sharedMaterials.Length).ToArray();
+            }
+            else
+            {
+                _renderer.sharedMaterial = mat;
+            }
         }
+        else
+        {
+            if (_renderer.materials.Length > 1)
+            {
+                _renderer.materials = Enumerable.Repeat(mat, _renderer.materials.Length).ToArray();
+            }
+            else
+            {
+                _renderer.material = mat;
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        _sub?.Dispose();
+        _sub = null;
     }
 }
